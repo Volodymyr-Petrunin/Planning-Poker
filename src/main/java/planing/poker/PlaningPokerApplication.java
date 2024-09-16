9b1239bd5167f4eb1a8ff190912ad2d855bc8978
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2024-09-16 13:58:58 +0200

    Init

---
+++ src/main/java/planing/poker/PlaningPokerApplication.java
@@ -0,0 +1,13 @@
+package planing.poker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaningPokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaningPokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/planing/poker/PlaningPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package planing.poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaningPokerApplicationTests {
+
+    @Test
+    void contextLoads() { // Noncompliant - method is empty
+    }
+
+}
