b0929c4fd77e35152b36f5a663c278912d1e2da2
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2024-10-08 15:14:13 +0200

    #16 Fix Mappers

--- src/main/java/planing/poker/mapper/RoomMapper.java
+++ src/main/java/planing/poker/mapper/RoomMapper.java
@@ -4,12 +4,9 @@ import org.mapstruct.*;
 import planing.poker.domain.Room;
 import planing.poker.domain.dto.RoomDto;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = {UserMapper.class, StoryMapper.class})
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface RoomMapper {
     Room toEntity(RoomDto roomDto);
 
     RoomDto toDto(Room room);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    Room partialUpdate(RoomDto roomDto, @MappingTarget Room room);
 }
--- src/main/java/planing/poker/mapper/VoteMapper.java
+++ src/main/java/planing/poker/mapper/VoteMapper.java
@@ -4,12 +4,9 @@ import org.mapstruct.*;
 import planing.poker.domain.Vote;
 import planing.poker.domain.dto.VoteDto;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = {UserMapper.class, StoryMapper.class})
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = {UserMapper.class})
 public interface VoteMapper {
     Vote toEntity(VoteDto voteDto);
 
     VoteDto toDto(Vote vote);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    Vote partialUpdate(VoteDto voteDto, @MappingTarget Vote vote);
 }
