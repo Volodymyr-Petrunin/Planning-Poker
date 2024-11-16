be0547a75425b79316e9faf60c3ffac86e73575b
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2024-11-16 18:42:13 +0100

    #123 Create validation for register

---
+++ src/main/java/planing/poker/common/validation/UniqueEmailValidator.java
@@ -0,0 +1,27 @@
+package planing.poker.common.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import planing.poker.repository.UserRepository;
+
+@Component
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UniqueEmailValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public boolean isValid(final String email, final ConstraintValidatorContext constraintValidatorContext) {
+        if (email == null) {
+            return true;
+        }
+
+        return !userRepository.existsByEmail(email);
+    }
+}
