950d08b86992f932180c39518bd205bcd2304a25
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2024-12-19 22:16:07 +0100

    #34 Refactor VoteServiceTest

--- src/test/java/planing/poker/service/VoteServiceTest.java
+++ src/test/java/planing/poker/service/VoteServiceTest.java
@@ -1,15 +1,24 @@
 package planing.poker.service;
 
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
+import com.navercorp.fixturemonkey.jakarta.validation.plugin.JakartaValidationPlugin;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import planing.poker.common.ExceptionMessages;
+import planing.poker.common.factory.EventMessageFactory;
 import planing.poker.domain.Vote;
+import planing.poker.domain.dto.request.RequestEventMessageDto;
 import planing.poker.domain.dto.request.RequestVoteDto;
+import planing.poker.domain.dto.response.ResponseUserDto;
 import planing.poker.domain.dto.response.ResponseVoteDto;
-import planing.poker.factory.utils.ExpectedEntityDtoUtils;
-import planing.poker.factory.utils.ExpectedEntityUtils;
+import planing.poker.mapper.StoryMapper;
+import planing.poker.mapper.UserMapper;
 import planing.poker.mapper.VoteMapper;
 import planing.poker.repository.VoteRepository;
 
@@ -17,7 +26,6 @@ import planing.poker.repository.VoteRepository;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -30,11 +38,26 @@ import static org.mockito.Mockito.doNothing;
 @DisplayName("Vote Service Tests")
 class VoteServiceTest {
 
-    private static final ResponseVoteDto EXPECTED_DTO = ExpectedEntityDtoUtils.getVote();
-    private static final Vote EXPECTED_ENTITY = ExpectedEntityUtils.getVote();
+    private static final FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
+            .objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
+            .plugin(new JakartaValidationPlugin())
+            .defaultNotNull(true)
+            .build();
+
+    private static ResponseVoteDto expectedResponseDto;
+
+    private static RequestVoteDto expectedRequestDto;
+
+    private static Vote expectedEntity;
 
     @InjectMocks
     private VoteService voteService;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private EventMessageService eventMessageService;
 
     @Mock
     private VoteRepository voteRepository;
@@ -42,88 +65,113 @@ class VoteServiceTest {
     @Mock
     private VoteMapper voteMapper;
 
+    @Mock
+    private ExceptionMessages exceptionMessages;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @Mock
+    private StoryMapper storyMapper;
+
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+
+    @Mock
+    private EventMessageFactory eventMessageFactory;
+
+    @BeforeAll
+    static void setUp() {
+        expectedResponseDto = fixtureMonkey.giveMeOne(ResponseVoteDto.class);
+        expectedRequestDto = fixtureMonkey.giveMeOne(RequestVoteDto.class);
+        expectedEntity = fixtureMonkey.giveMeOne(Vote.class);
+    }
+
     @Test
     @DisplayName("Create Vote: Should create vote and return correct DTO")
     void testCreateVote_ShouldCreateVote_AndReturnCorrectDto() {
-        final RequestVoteDto requestVoteDto = new RequestVoteDto();
-        when(voteMapper.toEntity(requestVoteDto)).thenReturn(EXPECTED_ENTITY);
-        when(voteRepository.save(EXPECTED_ENTITY)).thenReturn(EXPECTED_ENTITY);
-        when(voteMapper.toDto(EXPECTED_ENTITY)).thenReturn(EXPECTED_DTO);
+        when(userService.getUserByEmail(any())).thenReturn(fixtureMonkey.giveMeOne(ResponseUserDto.class));
+        when(voteMapper.toEntity(expectedRequestDto)).thenReturn(expectedEntity);
+        when(eventMessageFactory.createMessageVoteSubmittedIfVotingAnonymous(
+                expectedRequestDto.getEventId(),
+                expectedRequestDto.getVoter().getId(),
+                expectedRequestDto.getStory().getTitle())
+        ).thenReturn(fixtureMonkey.giveMeOne(RequestEventMessageDto.class));
+        when(voteRepository.save(expectedEntity)).thenReturn(expectedEntity);
+        when(voteMapper.toDto(expectedEntity)).thenReturn(expectedResponseDto);
 
-        final ResponseVoteDto createdVote = voteService.createVote(requestVoteDto, any());
+        final ResponseVoteDto createdVote = voteService.createVote(expectedRequestDto, any());
 
         assertNotNull(createdVote);
 
-        verify(voteRepository, times(1)).save(EXPECTED_ENTITY);
-        verify(voteMapper, times(1)).toEntity(requestVoteDto);
-        verify(voteMapper, times(1)).toDto(EXPECTED_ENTITY);
+        verify(voteRepository, times(1)).save(expectedEntity);
+        verify(voteMapper, times(1)).toEntity(expectedRequestDto);
+        verify(voteMapper, times(1)).toDto(expectedEntity);
     }
 
     @Test
     @DisplayName("Get All Votes: Should return all votes as a list of DTOs")
     void testGetAllVotes_ShouldReturnAllVotes() {
-        when(voteRepository.findAll()).thenReturn(List.of(EXPECTED_ENTITY));
-        when(voteMapper.toDto(EXPECTED_ENTITY)).thenReturn(EXPECTED_DTO);
+        when(voteRepository.findAll()).thenReturn(List.of(expectedEntity));
+        when(voteMapper.toDto(expectedEntity)).thenReturn(expectedResponseDto);
 
         final List<ResponseVoteDto> votes = voteService.getAllVotes();
 
         assertNotNull(votes);
 
         verify(voteRepository, times(1)).findAll();
-        verify(voteMapper, times(1)).toDto(EXPECTED_ENTITY);
+        verify(voteMapper, times(1)).toDto(expectedEntity);
     }
 
     @Test
     @DisplayName("Get Vote By ID: Should return the correct vote DTO for given ID")
     void testGetVoteById_ShouldReturnVote() {
-        when(voteRepository.findById(EXPECTED_DTO.getId())).thenReturn(Optional.of(EXPECTED_ENTITY));
-        when(voteMapper.toDto(EXPECTED_ENTITY)).thenReturn(EXPECTED_DTO);
+        when(voteRepository.findById(expectedResponseDto.getId())).thenReturn(Optional.of(expectedEntity));
+        when(voteMapper.toDto(expectedEntity)).thenReturn(expectedResponseDto);
 
-        final ResponseVoteDto vote = voteService.getVoteById(EXPECTED_DTO.getId());
+        final ResponseVoteDto vote = voteService.getVoteById(expectedResponseDto.getId());
 
         assertNotNull(vote);
 
-        verify(voteRepository, times(1)).findById(EXPECTED_DTO.getId());
-        verify(voteMapper, times(1)).toDto(EXPECTED_ENTITY);
+        verify(voteRepository, times(1)).findById(expectedResponseDto.getId());
+        verify(voteMapper, times(1)).toDto(expectedEntity);
     }
 
     @Test
     @DisplayName("Get Vote By ID: Should throw exception if vote is not found")
     void testGetVoteById_ShouldThrowExceptionIfNotFound() {
-        when(voteRepository.findById(EXPECTED_DTO.getId())).thenReturn(Optional.empty());
+        when(voteRepository.findById(expectedResponseDto.getId())).thenReturn(Optional.empty());
+        when(exceptionMessages.NO_FIND_MESSAGE()).thenReturn("Error not find!");
 
-        final Exception exception = assertThrows(
-                IllegalArgumentException.class, () -> voteService.getVoteById(EXPECTED_DTO.getId()));
+        assertThrows(IllegalArgumentException.class, () -> voteService.getVoteById(expectedResponseDto.getId()));
 
-        assertEquals("message.not.find.object", exception.getMessage());
-
-        verify(voteRepository, times(1)).findById(EXPECTED_DTO.getId());
+        verify(voteRepository, times(1)).findById(expectedResponseDto.getId());
     }
 
     @Test
     @DisplayName("Update Vote: Should update vote and return the updated DTO")
     void testUpdateVote_ShouldUpdateVote_AndReturnUpdatedDto() {
-        final RequestVoteDto requestVoteDto = new RequestVoteDto();
-        when(voteMapper.toEntity(requestVoteDto)).thenReturn(EXPECTED_ENTITY);
-        when(voteRepository.save(EXPECTED_ENTITY)).thenReturn(EXPECTED_ENTITY);
-        when(voteMapper.toDto(EXPECTED_ENTITY)).thenReturn(EXPECTED_DTO);
+        when(voteRepository.findById(1L)).thenReturn(Optional.of(expectedEntity));
+        when(voteMapper.toEntity(expectedRequestDto)).thenReturn(expectedEntity);
+        when(voteRepository.save(expectedEntity)).thenReturn(expectedEntity);
+        when(voteMapper.toDto(expectedEntity)).thenReturn(expectedResponseDto);
 
-        final ResponseVoteDto updatedVote = voteService.updateVote(1 ,requestVoteDto);
+        final ResponseVoteDto updatedVote = voteService.updateVote(1 ,expectedRequestDto);
 
         assertNotNull(updatedVote);
 
-        verify(voteRepository, times(1)).save(EXPECTED_ENTITY);
-        verify(voteMapper, times(1)).toEntity(requestVoteDto);
-        verify(voteMapper, times(1)).toDto(EXPECTED_ENTITY);
+        verify(voteRepository, times(1)).save(expectedEntity);
+        verify(voteMapper, times(1)).toEntity(expectedRequestDto);
+        verify(voteMapper, times(1)).toDto(expectedEntity);
     }
 
     @Test
     @DisplayName("Delete Vote: Should delete vote by given ID")
     void testDeleteVote_ShouldDeleteVote() {
-        doNothing().when(voteRepository).deleteById(EXPECTED_DTO.getId());
+        doNothing().when(voteRepository).deleteById(expectedResponseDto.getId());
 
-        voteService.deleteVote(EXPECTED_DTO.getId());
+        voteService.deleteVote(expectedResponseDto.getId());
 
-        verify(voteRepository, times(1)).deleteById(EXPECTED_DTO.getId());
+        verify(voteRepository, times(1)).deleteById(expectedResponseDto.getId());
     }
 }
